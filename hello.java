ff4a6ce9e0211f5b856445c914c5bcfa4c5ade3a
m-iwasaki <m-iwasaki@keyportsolutions.com>
2016-06-06 16:22:56 +0900

    [new]test

--- hello.java
+++ hello.java
@@ -3,6 +3,8 @@ package momo;
 public class hello {
 	 public static void main (String[] args) {
 	        System.out.println("Hello World !!");
-       	        System.out.println("テスト!!");
+       	        System.out.println("繝�繧ｹ繝�!!");
+       	     System.out.println("kyaaaaaaaa");
+    	        
 	    }
 }
